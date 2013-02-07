f65eeaa716e5d5547d898a1f873a0bf2f7a61db5
praveen.srinivasan <spraveen80@gmail.com>
2013-02-06 23:35:33 -0800

    Initial commit of travel rails 3 app
    Initial commit of travel rails 3 app. Most of the files are auto
    generated. The only files that were changed were Gemfile and
    .gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TravelOnRails3::Application.config.secret_token = '2bf8f34b3f6769db003a13fa8171d0fc6a74c523c58e2b6f4b5759d0fd185e6f980c925d22ba8587fe8d5f6dc4be6b63631ab43079d144e994f1c10df3190d3c'
